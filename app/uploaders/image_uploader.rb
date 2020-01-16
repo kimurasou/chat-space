c2a8a9e06ac20647a1e1eff1a099365d29a6d607
kimurasou <cb.102444@gmail.com>
2020-01-16 18:07:26 +0900

    [change]secrets.yml

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,15 +2,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_fit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/# {model.class.to_s.underscore}/# {mounted_as}/# {model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -30,7 +30,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [800, 800]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
